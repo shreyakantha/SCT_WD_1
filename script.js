e759aa4390d14e926460168bf1d194afa9a18d20
shreyakantha <jhinukKantha@gitt#23>
2025-11-13 20:54:16 +0530

    added JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+const nav = document.getElementById('main-nav');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
