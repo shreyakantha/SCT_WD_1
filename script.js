4426c0adea95e2a2eae179ab376d8922cae8004a
shreyakantha <shreyakantha348@gmail.com>
2026-07-26 23:18:52 +0530

    redesign project structure and merge css and js into html

--- script.js
+++
@@ -1,8 +0,0 @@
-const nav = document.getElementById('main-nav');
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
